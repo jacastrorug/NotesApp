e31306e6ee2a3fe727f2e028f1e027e96cd5e9db
jacastrorug <juliancastroruge@gmail.com>
2022-02-28 14:55:13 -0500

    Initial Commit

---
+++ NotesApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by Julian Castro Ruge on 28/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesApp WatchKit Extension/NotesAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesAppApp.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by Julian Castro Ruge on 28/02/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
